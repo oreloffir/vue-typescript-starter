9866d4fe39b8776d3a0f04efdcb035b9a0ed9011
orel offir <orel@compie.co.il>
2020-01-27 16:15:40 +0200

    Navigation init

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/example">Example</router-link>
+    <navigation id="nav" title="Main title"/>
+    <div id="view">
+      <router-view/>
     </div>
-    <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+  import Navigation from './components/layout/navigation';
 
-#nav {
-  padding: 30px;
+  export default {
+    name : 'App',
+    components : {
+      Navigation,
+    },
+  };
+</script>
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
